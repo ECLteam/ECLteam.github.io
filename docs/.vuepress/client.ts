6a347739c97060b418be936310655a87e7a13e40
Wuchang325 <kxxpg666@qq.com>
2026-09-21 13:29:28 +0800

    docs: add page context menu

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -3,6 +3,9 @@ import RepoCard from 'vuepress-theme-plume/features/RepoCard.vue'
 //import NpmBadge from 'vuepress-theme-plume/features/NpmBadge.vue'
 //import NpmBadgeGroup from 'vuepress-theme-plume/features/NpmBadgeGroup.vue'
 //import Swiper from 'vuepress-theme-plume/features/Swiper.vue'
+import { h } from 'vue'
+import { Layout } from 'vuepress-theme-plume/client'
+import PageContextMenu from 'vuepress-theme-plume/features/PageContextMenu.vue'
 
 import '@/theme/styles/custom.css'
 import '@/theme/styles/index.css'
@@ -17,12 +20,18 @@ export default defineClientConfig({
 
     // your custom components
     // app.component('CustomComponent', CustomComponent)
-    
+
   },
   setup() {
     if (typeof window !== 'undefined') {
       console.log('客户端环境初始化')
     }
+  },
+  layouts: {
+    Layout: h(Layout, null, {
+      // 将 PageContextMenu 添加到 doc-title-after 插槽，即文章标题的右侧
+      'doc-title-after': () => h(PageContextMenu),
+    }),
   },
   rootComponents: [
   ],
